7e8addf07610de938064d7393a0e115d2898b09f
Austin <christopheraustinwallace@gmail.com>
2023-10-19 15:56:47 -0400

    Add getComputerChoice function
    created the getComputerChoice function, which randomly returns the string: rock, paper, or scissors.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,1 +1,15 @@
-console.log('Testing')
+// 1) getComputerChoice will randomly return rock, paper, or scissors
+function getComputerChoice() {
+    let randomNum = Math.floor(Math.random() * 3);
+    let computerChoice;
+
+    if (randomNum == 0) {
+        return "rock";
+    } else if (randomNum == 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice())
