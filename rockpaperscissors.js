b3babd295d55ec5064ccea9b39df6934e44f7e5c
Austin <christopheraustinwallace@gmail.com>
2023-10-19 16:12:29 -0400

    Create playRound function
    Created playRound function to simulate the computer playing a round of rock, paper, scissors with the human

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -12,4 +12,32 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+    let playerSelectionLower = playerSelection.toLowerCase()
+    if (playerSelectionLower === computerSelection) {
+        return "It's a draw!"
+    } else if (playerSelectionLower === "rock") {
+        if (computerSelection === "paper") {
+            return "You lose! Paper beats Rock"
+        } else {
+            return "You win! Rock beats Scissors"
+        }
+    } else if (playerSelectionLower === "paper") {
+        if (computerSelection === "scissors") {
+            return "You lose! Scissors beats paper"
+        } else {
+            return "You win! Paper beats Rock"
+        }
+    } else if (playerSelectionLower === "scissors") {
+        if (computerSelection === "rock") {
+            return "You lose! Rock beats Scissors"
+        } else {
+            return "You win! Scissors beats Rock"
+        }
+    }
+}
+const playerSelection = 'paper';
+const computerSelection = getComputerChoice()
+console.log("player selection: " + playerSelection)
+console.log("computerSelection: " + computerSelection)
+console.log(playRound(playerSelection, computerSelection))
